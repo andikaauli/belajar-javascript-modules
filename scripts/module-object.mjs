8ec1583ac2a3b8e23ea912f2c6243a4802dd499e
Andika Auli <dikaauli25@gmail.com>
2024-07-31 17:14:06 +0700

    [FEAT] module pbject

---
+++ scripts/module-object.mjs
@@ -0,0 +1,17 @@
+export const firstName = "Dika";
+export const middleName = "Aulia";
+export const lastName = "Auli";
+
+
+export function hello(){
+    console.info("Hello from dika")
+}
+
+export class Person {
+    constructor(name){
+        this.name = "Dika"
+    }
+    // sayHello(name){
+    //     console.info(`Hello ${name}, my name is ${this.name}`)
+    // }
+}
